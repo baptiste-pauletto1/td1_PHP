67933d9a3c9df1f574de69ed33bea54835b86bb0
p16005334 <baptiste-pauletto@etu.univ-amu.fr>
2017-12-07 10:55:25 +0100

    TD1 terminé !

---
+++ calcul.php
@@ -0,0 +1,47 @@
+<?php
+    include_once ('fonctions.php');
+    start_page('ici, ça calcule');
+?>
+
+<?php
+    $op1 = $_POST['op1'];
+    $op2 = $_POST['op2'];
+    $action = $_POST['action'];
+
+    echo "$op1 $action $op2 = ";
+
+
+    if('*' == $action)
+    {
+        $resultat = $op1 * $op2;
+    }
+    elseif('+' == $action)
+    {
+        $resultat = $op1 + $op2;
+    }
+    elseif('-' == $action)
+    {
+        $resultat = $op1 - $op2;
+    }
+    elseif('/' == $action)
+    {
+        if ($op2 == 0) {
+            echo '<strong>tu déconnes Billy, on divise pas par 0 !</strong></br>';
+        }
+        else
+        $resultat = $op1 / $op2;
+    }
+    else
+    {
+        echo '<br/><strong>opérateur ' . $action . ' non géré </strong>';
+    }
+
+    echo $resultat;
+
+?>
+
+
+
+<?php
+    end_page();
+?>
--- calculator.php
+++ calculator.php
@@ -1,18 +1,27 @@
 <?php
     include_once('fonctions.php');
     start_page('Super calculateur');
+    $operateurs = '*+-/';
 ?>
 
-    <form action="calcul.php" method="post">
-        <p><input type="text" name="op1"></p> </br>
-        <p><input type="text" name="op2"></p> </br>
-    <input checked="checked" type="radio" name="op" value="*"/>*<br/>
-    <input type="radio" name="op" value="+"/>+<br/>
-    <input type="radio" name="op" value="-"/>-<br/>
-    <input type="radio" name="op" value="/"/>/<br/>
-        <input type ="submit" value="Calculer" />
+    <form action="calcul.php" method="get">
+        <p><input type="text" name="op1"></p>
+        <p><input type="text" name="op2"></p>
+        <?php
+        for($cpt = 0 ; $cpt <= 3 ; ++$cpt)
+        {
+            echo '<input ';
+            if($cpt == 0)
+            {
+                echo 'checked="checked" ';
+            }
+            echo 'type="submit" name="action" value="' . $operateurs[$cpt] . '"/> ' . "\n";
+            }
+        ?>
         <input type ="reset" value="Effacer" />
     </form>
+
+
 <?php
     end_page();
 ?>
